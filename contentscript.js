a9e5d37526a71d347c606030f02e43cf5ea4ce60
Florian <florian@backbase.com>
2021-11-04 15:22:11 +0100

    Improve plugin by reentering in the room while canceling

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,41 @@
+// Sources: https://gist.github.com/florianpasteur/118d0e29313c3fb052f944bc001cde88
+function findElementByText(text, searchStart = document.body, _document = document, ignoreSpace = false) {
+    return _document
+        .evaluate(
+            `//*[${ignoreSpace ? 'normalize-space' : 'text'}()="${text}"]`,
+            searchStart,
+            null,
+            XPathResult.ANY_TYPE,
+            null
+        )
+        .iterateNext();
+}
+
 window.onbeforeunload = function(event){
     event.preventDefault();
     console.log(event);
 
-    return `You're about to leave the meeting`;
+    setInterval(() => {
+        window.onbeforeunload = null;
+        window.location.href = window.location.href + '?rejoin=true'
+    }, 100);
+
+    return event.returnValue = `You're about to leave the meeting`;
 };
 
+
+if (window.location.href.indexOf('rejoin=true') >= 0) {
+    let nbOfAttempt = 0;
+    console.log("Trying to rejoin");
+    let watchProcess = setInterval(() => {
+        if (!findElementByText('Getting ready...')) {
+            findElementByText('Join now').click();
+            clearInterval(watchProcess);
+        }
+        if (nbOfAttempt++ < 25) {
+            clearInterval(watchProcess);
+        }
+
+        console.log(".");
+    }, 250);
+}
